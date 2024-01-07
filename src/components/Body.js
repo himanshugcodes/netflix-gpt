66e7eec00127e6a0e3fd96a35f4204473947a95b
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2024-01-08 00:06:08 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../components/Login";
+
+const Body = () => {
+  return (
+    <div>
+      <Login />
+      <h1> Body</h1>
+    </div>
+  );
+};
+
+export default Body;
